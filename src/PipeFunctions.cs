39edf4b09369600f39f3b0c2af50f1dcca9f80c7
沈伟 <330589465@qq.com>
2020-05-29 15:26:45 +0800

    添加项目文件。

---
+++ src/PipeFunctions.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace Functional.PipeFuncion
+{
+    public static class PipeFunctions
+    {
+        public delegate TResult RefStructFunc<T, out TResult>(ref T it) where T : struct;
+
+        public delegate void RefStructAction<T>(ref T it) where T : struct;
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static TResult Let<T, TResult>(this T @this, Func<T, TResult> func)
+        {
+            return func(@this);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void Let<T>(this T @this, Action<T> func)
+        {
+            func(@this);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static T Also<T>(this T @this, Action<T> func)
+        {
+            func(@this);
+            return @this;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static TResult Let<T, TResult>(this ref T @this, RefStructFunc<T, TResult> func) where T : struct
+        {
+            return func(ref @this);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void Let<T>(this ref T @this, RefStructAction<T> func) where T : struct
+        {
+            func(ref @this);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static T Also<T>(this ref T @this, RefStructAction<T> func) where T : struct
+        {
+            func(ref @this);
+            return @this;
+        }
+    }
+}
