8259a2b79df5f089fa0e5cb267d887e013e0df4e
沈伟 <330589465@qq.com>
2021-02-07 17:29:05 +0800

    调整一下目录,增加单元测试

---
+++ test/Functional.PipeFunction.Tests/EnumberableExtensionsTests.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+using Functional.PipeFunction;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Shouldly;
+
+namespace Functional.PipeFunction.Tests
+{
+    [TestFixture()]
+    public class EnumberableExtensionsTests
+    {
+        [Test()]
+        public void FirstOrNullTestWithClass()
+        {
+            new string[0].FirstOrNull().ShouldBeNull();
+            new string[] { "1" }.FirstOrNull(it => it == "2").ShouldBeNull();
+        }
+
+        [Test()]
+        public void FirstOrNullTestWithNullable()
+        {
+            new int?[0].FirstOrNull().ShouldBeNull();
+            new int?[] { 1 }.FirstOrNull(it => it == 2).ShouldBeNull();
+        }
+
+        [Test()]
+        public void FirstOrNullTestWithStruct()
+        {
+            new int[0].FirstOrNull().ShouldBeNull();
+            new int[] { 1 }.FirstOrNull(it => it == 2).ShouldBeNull();
+        }
+    }
+}
---
+++ test/Functional.PipeFunction.Tests/PipeFunctionsTests.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using Functional.PipeFunction;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Shouldly;
+
+namespace Functional.PipeFunction.Tests
+{
+    [TestFixture()]
+    public class PipeFunctionsTests
+    {
+        [Test()]
+        public void ToTest()
+        {
+            "4".Let(int.Parse)
+               .Let(it => Math.Sqrt(it))
+               .To(out var result);
+
+            result.ShouldBe(2);
+        }
+    }
+}
